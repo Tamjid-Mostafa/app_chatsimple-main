db8ec8f4f7ce8962c40eb1654afb6fcae3b535d0
Jahid Hasan <jahidhasan2856@gmail.com>
2023-04-19 15:47:25 +0600

    connected api with create chatboat title

--- src/components/chatbot-tabs/Chatbot_tab_2_new.jsx
+++ src/components/chatbot-tabs/Chatbot_tab_2_new.jsx
@@ -1,111 +1,123 @@
 import { TextField, Switch } from '@mui/material';
-import { Box, Typography, IconButton } from '@mui/material';
-import displayimg from "../../assets/chatbot_display_img.png";
-import { allChatBots, createChatBot } from "../../redux/reducers/chatbotSlice";
-import { useState } from 'react';
-
-import ConversationCard from './ConversationCard';
-import { useDispatch, useSelector } from "react-redux";
+import displayimg from '../../assets/chatbot_display_img.png';
+import { createChatBot } from '../../redux/reducers/chatbotSlice';
+import React, { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Chatbot_tab_2_new = ({ changeChatBotTab }) => {
-    const dispatch = useDispatch();
-    const { user } = useSelector((state) => state.user);
-    const { chatBots } = useSelector((state) => state.chatbot); // this has all the chat-bots list for a user that is logged in
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
+  const { chatBots } = useSelector((state) => state.chatbot); // this has all the chat-bots list for a user that is logged in
+  const [isTyping, setIsTyping] = useState(false);
+  const [chatbotTitle, setChatbotTitle] = useState('');
+  const [prevTitle, setPrevTitle] = useState('');
 
-    const [chatbotTitle, setChatbotTitle] = useState("");
+  console.log({ chatBots });
 
-    const chatbotHandler = () => {
-        // for chat bot creation
-        if (!!chatbotTitle) {
-            const data = {
-                userID: user?.user_id,
-                chatbotDetail: {
-                    timezone: "UTC",
-                    platforms: { first: "messenger" },
-                    chatbot_title: chatbotTitle,
-                },
-                chatbotID: uuidv4(),
-            };
-            dispatch(createChatBot(data));
-        }
+  const chatbotHandler = (title) => {
+    // for chat bot creation
+
+    const data = {
+      userID: 1671593203311000,
+      //   userID: user?.user_id,
+      chatbotDetail: {
+        timezone: 'UTC',
+        platforms: { first: 'messenger' },
+        chatbot_title: title,
+      },
+      chatbotID: uuidv4(),
     };
+    dispatch(createChatBot(data));
+  };
 
-    const [isChecked, setIsChecked] = useState(false);
+  useEffect(() => {
+    if (!prevTitle) {
+      if (!isTyping && !!chatbotTitle) {
+        setPrevTitle(chatbotTitle);
+        chatbotHandler(chatbotTitle);
+        console.log('create');
+      }
+    } else if (prevTitle !== chatbotTitle && !isTyping) {
+      console.log('update');
+    }
+  }, [isTyping, chatbotTitle]);
 
-    const handleToggle = () => {
-        setIsChecked(!isChecked);
-    };
-    return (
-        <div className='display_flex'>
-            <div>
-                <div className='chatbot_header_top'>
-                    <h2 className='bold_text'>Name your Chatboat</h2>
-                    <TextField
-                        label="Name"
-                        variant="outlined"
-                        onChange={(e) => setChatbotTitle(e.target.value)}
-                        value={chatbotTitle}
-                    />
-                     <div className=''>
-                        <button className='text-sm text-white px-5 bg-[#66B467] py-2 rounded-full margintop' onClick={chatbotHandler}>
-                            Create
-                        </button>
-                    </div>
-                </div>
+  const handleBlur = () => {
+    setIsTyping(false);
+  };
 
-                <div>
-                    <h2 className='bold_text'>Select Chatbot Expertise</h2>
+  const handleFocus = () => {
+    setIsTyping(true);
+  };
 
-                    <div className='expertise_box display_flex'>
-                        <div className='faq_text'>
-                            <p> FAQ</p>
-                        </div>
-                        <div className='chatbot_toggle_button'>
-                            <Switch 
-                            onClick={() => changeChatBotTab(3)}
-                            />
-                        </div>
-                    </div>
+  const [isChecked, setIsChecked] = useState(false);
 
-                    <div className='expertise_box display_flex'>
-                        <div className='faq_text'>
-                            <p> Business small talk</p>
-                        </div>
-                        <div className='chatbot_toggle_button2'>
-                            <Switch
-                             onClick={() => changeChatBotTab(4)}
-                             />
-                        </div>
-                    </div>
-
-                    <div className='expertise_box display_flex'>
-                        <div className='faq_text'>
-                            <p>Business Goal</p>
-                        </div>
-                        <div className='chatbot_toggle_button3'>
-                            <Switch
-                             onClick={() => changeChatBotTab(5)}
-                             />
-                        </div>
-                    </div>
-
-                </div>
-
-            </div >
-            <div className='chatbot_dsplay_column'>
-                <div className='chatbot_display_text'>
-                   <h1 className='bold_text font_32'>Discover the world of chat simple</h1>
-                </div>
-            <div className='chatbot_display_img_style'>
-            <img src={displayimg} alt="My Image" />
-            </div>
-
-            <div className='chatbot_display_img_style'>
-            <img src={displayimg} alt="My Image" />
-            </div>
-            </div>
+  const handleToggle = () => {
+    setIsChecked(!isChecked);
+  };
+  return (
+    <div className='display_flex'>
+      <div>
+        <div className='chatbot_header_top'>
+          <h2 className='bold_text'>Name your Chatboat</h2>
+          <TextField
+            label='Name'
+            variant='outlined'
+            onBlur={handleBlur}
+            onFocus={handleFocus}
+            onChange={(e) => setChatbotTitle(e.target.value)}
+            value={chatbotTitle}
+          />
         </div>
-    )
-}
+
+        <div>
+          <h2 className='bold_text'>Select Chatbot Expertise</h2>
+
+          <div className='expertise_box display_flex'>
+            <div className='faq_text'>
+              <p> FAQ</p>
+            </div>
+            <div className='chatbot_toggle_button'>
+              <Switch onClick={() => changeChatBotTab(3)} />
+            </div>
+          </div>
+
+          <div className='expertise_box display_flex'>
+            <div className='faq_text'>
+              <p> Business small talk</p>
+            </div>
+            <div className='chatbot_toggle_button2'>
+              <Switch onClick={() => changeChatBotTab(4)} />
+            </div>
+          </div>
+
+          <div className='expertise_box display_flex'>
+            <div className='faq_text'>
+              <p>Business Goal</p>
+            </div>
+            <div className='chatbot_toggle_button3'>
+              <Switch onClick={() => changeChatBotTab(5)} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='chatbot_dsplay_column'>
+        <div className='chatbot_display_text'>
+          <h1 className='bold_text font_32'>
+            Discover the world of chat simple
+          </h1>
+        </div>
+        <div className='chatbot_display_img_style'>
+          <img src={displayimg} alt='My Image' />
+        </div>
+
+        <div className='chatbot_display_img_style'>
+          <img src={displayimg} alt='My Image' />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Chatbot_tab_2_new;
