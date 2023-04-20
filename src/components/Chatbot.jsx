f02eb5a96117fd9869936bfab0248b89039eeae8
Jahid Hasan <jahidhasan2856@gmail.com>
2023-04-20 17:05:38 +0600

    changes react toast used alert component

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -1,37 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import Chatbot_tab_1 from './chatbot-tabs/Chatbot_tab_1'
-import Chatbot_tab_2 from './chatbot-tabs/Chatbot_tab_2'
-import Chatbot_tab_2_new from './chatbot-tabs/Chatbot_tab_2_new'
-import Chatbot_faq_details from './chatbot-tabs/Chatbot_faq_details'
-import Chatbot_business_talk from './chatbot-tabs/Chatbot_business_talk'
-import Chatbot_business_goal from './chatbot-tabs/Chatbot_business_goal'
-import Chatbot_tab_3 from './chatbot-tabs/Chatbot_tab_3'
-import ChatbotConnectToChannel from './ChatbotConnectToChannel/ChatbotConnectToChannel'
-import ChatbotConnectToChannelFirst from './ChatbotConnectToChannel/ChatbotConnectToChannelFirst'
-import Chatbotfinish from './Chatbotfinish/Chatbotfinish'
-import CreateChatbotLast from './CreateChatbotLast/CreateChatbotLast'
-import { useDispatch, useSelector } from 'react-redux'
-import { Switch, TextField } from '@mui/material'
-import { createChatBot, updateChatBot } from '../redux/reducers/chatbotSlice'
+import React, { useEffect, useState } from 'react';
+import Chatbot_tab_1 from './chatbot-tabs/Chatbot_tab_1';
+import Chatbot_tab_2 from './chatbot-tabs/Chatbot_tab_2';
+import Chatbot_tab_2_new from './chatbot-tabs/Chatbot_tab_2_new';
+import Chatbot_faq_details from './chatbot-tabs/Chatbot_faq_details';
+import Chatbot_business_talk from './chatbot-tabs/Chatbot_business_talk';
+import Chatbot_business_goal from './chatbot-tabs/Chatbot_business_goal';
+import Chatbot_tab_3 from './chatbot-tabs/Chatbot_tab_3';
+import ChatbotConnectToChannel from './ChatbotConnectToChannel/ChatbotConnectToChannel';
+import ChatbotConnectToChannelFirst from './ChatbotConnectToChannel/ChatbotConnectToChannelFirst';
+import Chatbotfinish from './Chatbotfinish/Chatbotfinish';
+import CreateChatbotLast from './CreateChatbotLast/CreateChatbotLast';
+import { useDispatch, useSelector } from 'react-redux';
+import { Switch, TextField } from '@mui/material';
+import { createChatBot, updateChatBot } from '../redux/reducers/chatbotSlice';
 import { v4 as uuidv4 } from 'uuid';
-import { toast } from 'react-hot-toast';
-
-import Topbar from './Topbar/Topbar'
+import ActionAlert from './Alert/ActionAlert';
 
 const Chatbot = () => {
-  const [chatbotTab, setChatbotTab] = React.useState(1)
+  const [chatbotTab, setChatbotTab] = React.useState(1);
   const { loading, user: userData } = useSelector((state) => {
-    return state.auth
-  })
+    return state.auth;
+  });
   useEffect(() => {
     if (!loading && !userData) {
       // window.alert("Please login to continue")
     } else {
     }
-  }, [userData, loading])
+  }, [userData, loading]);
   const changeChatBotTab = (tab) => {
-    setChatbotTab(tab)
-  }
+    setChatbotTab(tab);
+  };
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.user);
   const { chatBots } = useSelector((state) => state.chatbot); // this has all the chat-bots list for a user that is logged in
@@ -40,6 +38,10 @@ const Chatbot = () => {
   const [prevTitle, setPrevTitle] = useState('');
   const [chatBotID, setChatBotID] = useState('');
   const [platforms, setPlatforms] = useState([]);
+
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
     if (!prevTitle) {
@@ -58,7 +60,8 @@ const Chatbot = () => {
         };
         setChatBotID(data.chatbotID);
         dispatch(createChatBot(data));
-        toast.success('Chatbot name created')
+        setSuccess(true);
+        setMessage('Chatbot name created')
       }
     } else if (prevTitle !== chatbotTitle && !isTyping) {
       setPrevTitle(chatbotTitle);
@@ -70,19 +73,16 @@ const Chatbot = () => {
         update_mask: 'chatbot_title',
       };
       dispatch(updateChatBot(data));
-      toast.success('Chatbot name updated')
+      setSuccess(true);
+      setMessage('Chatbot name updated')
     }
   }, [isTyping, chatbotTitle]);
-
-
 
   const handlePlatform = (value) => {
     let copyPlatform = platforms;
 
-    if(!chatBotID){
-
-      toast.error('Create chatbot name')
-      return
+    if (!chatBotID) {
+      return;
     }
 
     if (value === 'messanger') {
@@ -107,15 +107,18 @@ const Chatbot = () => {
 
     const data = {
       userID: user?.user_id,
-      update: { platforms: copyPlatform?.length === 0 ? null :  { first: copyPlatform[0], scond: copyPlatform[1] } },
+      update: {
+        platforms:
+          copyPlatform?.length === 0
+            ? null
+            : { first: copyPlatform[0], scond: copyPlatform[1] },
+      },
       chatbotID: chatBotID,
       update_mask: 'platforms',
     };
 
     dispatch(updateChatBot(data));
-
   };
-
 
   const handleBlur = () => {
     setIsTyping(false);
@@ -125,27 +128,37 @@ const Chatbot = () => {
     setIsTyping(true);
   };
 
+
+  console.log(success)
   return (
     <>
-    <div className='w-full flex'>
-      {chatbotTab === 1 && <Chatbot_tab_1 changeChatBotTab={changeChatBotTab} user={userData} />}
-      <div className={`${chatbotTab !== 1 ? "" : "hidden"}`}>
+      {success && (
+        <ActionAlert
+          variant='filled'
+          severity='success'
+          message={message}
+          setData={setMessage}
+        />
+      )}
+      <div className='w-full flex'>
+        {chatbotTab === 1 && (
+          <Chatbot_tab_1 changeChatBotTab={changeChatBotTab} user={userData} />
+        )}
+        <div className={`${chatbotTab !== 1 ? '' : 'hidden'}`}>
+          <div className='p-5 h-[calc(100vh-50px)]'>
+            <div className='chatbot_header_top'>
+              <h2 className='bold_text'>Name your Chatbot</h2>
+              <TextField
+                label='Name'
+                variant='outlined'
+                onBlur={handleBlur}
+                onFocus={handleFocus}
+                onChange={(e) => setChatbotTitle(e.target.value)}
+                value={chatbotTitle}
+              />
+            </div>
 
-        <div className='p-5 h-[calc(100vh-50px)]'>
-          <div className='chatbot_header_top'>
-            <h2 className='bold_text'>Name your Chatbot</h2>
-            <TextField
-              label='Name'
-              variant='outlined'
-              onBlur={handleBlur}
-              onFocus={handleFocus}
-              onChange={(e) => setChatbotTitle(e.target.value)}
-              value={chatbotTitle}
-            />
-          </div>
-
-
-          <div>
+            <div>
               <h2 className='bold_text'>Display to connect channel(s)</h2>
 
               <div className=' expertise_box display_flex justify-between'>
@@ -163,7 +176,7 @@ const Chatbot = () => {
                 </div>
               </div>
 
-              <div  className=' expertise_box display_flex justify-between'>
+              <div className=' expertise_box display_flex justify-between'>
                 <div
                   onClick={() => handlePlatform('instagram')}
                   className='faq_text cursor-pointer'
@@ -177,53 +190,90 @@ const Chatbot = () => {
                   />
                 </div>
               </div>
-
-
             </div>
 
-          <div className='mt-5'>
-            <h2 className='bold_text'>Select Chatbot Expertise</h2>
+            <div className='mt-5'>
+              <h2 className='bold_text'>Select Chatbot Expertise</h2>
 
-            <div className='expertise_box display_flex'>
-              <div onClick={() => changeChatBotTab(3)} className='faq_text cursor-pointer'>
-                <p> FAQ</p>
+              <div className='expertise_box display_flex'>
+                <div
+                  onClick={() => changeChatBotTab(3)}
+                  className='faq_text cursor-pointer'
+                >
+                  <p> FAQ</p>
+                </div>
+                <div className='chatbot_toggle_button'>
+                  <Switch onClick={() => changeChatBotTab(3)} />
+                </div>
               </div>
-              <div className='chatbot_toggle_button'>
-                <Switch onClick={() => changeChatBotTab(3)} />
-              </div>
-            </div>
 
-            <div className='expertise_box display_flex'>
-              <div onClick={() => changeChatBotTab(4)} className='faq_text cursor-pointer'>
-                <p> Business small talk</p>
+              <div className='expertise_box display_flex'>
+                <div
+                  onClick={() => changeChatBotTab(4)}
+                  className='faq_text cursor-pointer'
+                >
+                  <p> Business small talk</p>
+                </div>
+                <div className='chatbot_toggle_button2'>
+                  <Switch onClick={() => changeChatBotTab(4)} />
+                </div>
               </div>
-              <div className='chatbot_toggle_button2'>
-                <Switch onClick={() => changeChatBotTab(4)} />
-              </div>
-            </div>
 
-            <div className='expertise_box display_flex'>
-            <div onClick={() => changeChatBotTab(5)} className='faq_text cursor-pointer'>
-                <p>Business Goal</p>
-              </div>
-              <div className='chatbot_toggle_button3'>
-                <Switch onClick={() => changeChatBotTab(5)} />
+              <div className='expertise_box display_flex'>
+                <div
+                  onClick={() => changeChatBotTab(5)}
+                  className='faq_text cursor-pointer'
+                >
+                  <p>Business Goal</p>
+                </div>
+                <div className='chatbot_toggle_button3'>
+                  <Switch onClick={() => changeChatBotTab(5)} />
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <div className='bg-white w-[100vw]'>
+          {chatbotTab === 2 && (
+            <Chatbot_tab_2_new
+              changeChatBotTab={changeChatBotTab}
+              user={userData}
+            />
+          )}
+          {chatbotTab === 3 && (
+            <Chatbot_faq_details
+              changeChatBotTab={changeChatBotTab}
+              user={userData}
+            />
+          )}
+          {chatbotTab === 4 && (
+            <Chatbot_business_talk
+              changeChatBotTab={changeChatBotTab}
+              user={userData}
+            />
+          )}
+          {chatbotTab === 5 && (
+            <Chatbot_business_goal
+              changeChatBotTab={changeChatBotTab}
+              user={userData}
+            />
+          )}
+          {chatbotTab === 7 && (
+            <Chatbotfinish
+              changeChatBotTab={changeChatBotTab}
+              user={userData}
+            />
+          )}
+          {chatbotTab === 8 && (
+            <CreateChatbotLast
+              changeChatBotTab={changeChatBotTab}
+              user={userData}
+            />
+          )}
+        </div>
       </div>
-      <div className='bg-white w-[100vw]'>
-        {chatbotTab === 2 && <Chatbot_tab_2_new changeChatBotTab={changeChatBotTab} user={userData} />}
-        {chatbotTab === 3 && <Chatbot_faq_details changeChatBotTab={changeChatBotTab} user={userData} />}
-        {chatbotTab === 4 && <Chatbot_business_talk changeChatBotTab={changeChatBotTab} user={userData} />}
-        {chatbotTab === 5 && <Chatbot_business_goal changeChatBotTab={changeChatBotTab} user={userData} />}
-        {chatbotTab === 7 && <Chatbotfinish changeChatBotTab={changeChatBotTab} user={userData} />}
-        {chatbotTab === 8 && <CreateChatbotLast changeChatBotTab={changeChatBotTab} user={userData} />}
-      </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Chatbot
+export default Chatbot;
