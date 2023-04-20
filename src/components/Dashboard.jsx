74373be08083f07b6a0c6d72bba07978ee1009c5
dev-abusalem <dev.abusalem@gmail.com>
2023-04-20 13:38:17 +0600

    Topbar and Thank you page

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,6 +3,7 @@ import DB_Tab_1 from './dashboard-tabs/DB_Tab_1'
 import HomePage from './HomePage/HomePage'
 import HomePageV2 from './HomePage/HomePageV2'
 import { useSelector } from 'react-redux'
+import Topbar from './Topbar/Topbar'
 
 const DashboardC = () => {
   const [dashboardTab, setDashboardTab] = React.useState(2)
@@ -19,11 +20,14 @@ const DashboardC = () => {
   const changeDashboardTab = (tab) => {
     setDashboardTab(tab)
   }
-  // 
+  //
   return (
     <div>
-      {/* {dashboardTab === 1 && <HomePage changeDashboardTab = {changeDashboardTab} />} */}
-      {dashboardTab === 2 && <HomePageV2 changeDashboardTab = {changeDashboardTab} user = {userData} />}
+      <Topbar />
+      <div className='p-5'>
+        {/* {dashboardTab === 1 && <HomePage changeDashboardTab = {changeDashboardTab} />} */}
+        {dashboardTab === 2 && <HomePageV2 changeDashboardTab = {changeDashboardTab} user = {userData} />}
+      </div>
     </div>
   )
 }
